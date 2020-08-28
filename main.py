2636c961431d74db035736372f84c324876cc327
rishabhsaxena01 <rishabh.saxena01@nagarro.com>
2020-08-28 21:13:45 +0530

    intial wireframe is ready

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
 import numpy as np
+import pandas as pd
+
+#<----------------------------------------  problem preparation  ------------------------------------------>
+
+#Create the graph
+
+#Draw the graph
+
+
+#<-----------------------------------------------ACO algo--------------------------------------------------> 
+
+#initial parameters of ACO
+
+max_iterations = 100
+
+#main loop for ACO
+
+for i in range(max_iterations):
+    #create the ants 
+
+
+    #calculate the fitness value of all the ants 
+
+
+    #find the best ant(queen)
+
+
+    #update the pheromone matrix
+
+
+    #evaporation
+
+
+    # display the result
+
+    return
